a8c8dc1938adae9ac88989e668d44c878a19e9f0
GuessEver <guessever@gmail.com>
2017-12-22 15:55:04 +0800

    for test

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of TeamELF
+ *
+ * (c) GuessEver <guessever@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use TeamELF\Application\Server as ServerApplication;
+
+$app = new ServerApplication(__DIR__ . '/../');
+$app->start();
